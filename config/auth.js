e2582cf9dff425c4577710f3d5bb499a262b10fe
kashif <irshadkashifi.r@gmail.com>
2020-11-09 12:24:18 +0100

    first commit

--- config/auth.js
+++ config/auth.js
@@ -5,11 +5,19 @@ module.exports = {
         'callbackURL': 'http://localhost:3000/api/auth/facebook/callback'
     },
 
-    "development": {
+    /* "development": {
         "username": "kashif95",
         "password": "kashif1512",
         "database": "kashif",
         "host": "ca0c62d7-5a74-44a1-b708-f926b1c40312.kashif-7913.mysql.dbs.scalingo.com",
+        "dialect": "mysql",
+        "operatorsAliases": false
+    }, */
+    "development": {
+        "username": "root",
+        "password": "",
+        "database": "nodeshop",
+        "host": "127.0.0.1",
         "dialect": "mysql",
         "operatorsAliases": false
     },
